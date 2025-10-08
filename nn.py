286449684b8103d1b7b8482052a2577e6b290854
Null_Byte_404 <theabhilashsingh@gmail.com>
2025-10-08 15:16:25 +0530

    Delete nn.py

--- nn.py
+++
@@ -1,9 +0,0 @@
-students = [
-    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Draco", "house": "Slytherin", "patronus": None},
-    {"name": "Luna", "house": "Ravenclaw", "patronus": "Hare"},
-    {"name": "Cedric", "house": "Hufflepuff", "patronus": "Badger"},
-
-]
-for student in students:
-    print(f"{student['name']} is in {student['house']} and their patronus is {student['patronus']}")
