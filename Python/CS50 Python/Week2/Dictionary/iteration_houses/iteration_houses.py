3e851909a265d5a94671b088ae895889597653b5
Abhilash5542 <abhilashsingh85173@gmail.com>
2025-10-08 11:56:15 +0530

    iteration_houses

---
+++ Python/CS50 Python/Week2/Dictionary/iteration_houses/iteration_houses.py
@@ -0,0 +1,6 @@
+students = {"Harry Potter": "Gryffindor",
+            "Hermione Granger": "Gryffindor",
+            "Ron Weasley": "Gryffindor",
+            "Draco Malfoy": "Slytherin",}
+for student in students:
+    print(student , students[student],sep=": ")
