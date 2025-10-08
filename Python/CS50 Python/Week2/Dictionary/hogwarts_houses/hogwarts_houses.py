d99b538af18f013dd2449f3038b11b8e599b9145
Abhilash5542 <abhilashsingh85173@gmail.com>
2025-10-08 11:49:12 +0530

    hogwarts_houses

---
+++ Python/CS50 Python/Week2/Dictionary/hogwarts_houses/hogwarts_houses.py
@@ -0,0 +1,8 @@
+students = {"Harry Potter": "Gryffindor",
+            "Hermione Granger": "Gryffindor",
+            "Ron Weasley": "Gryffindor",
+            "Draco Malfoy": "Slytherin",}
+print(students["Harry Potter"]) # Output: Gryffindor
+print(students["Hermione Granger"]) # Output: Gryffindor
+print(students["Ron Weasley"]) # Output: Gryffindor
+print(students["Draco Malfoy"]) # Output: Slytherin
