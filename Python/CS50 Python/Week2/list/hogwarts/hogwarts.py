7dac536cbcd1219f9b4ff81bf2b6c2a38998e22f
Abhilash5542 <abhilashsingh85173@gmail.com>
2025-10-08 11:28:54 +0530

    hogwarts

---
+++ Python/CS50 Python/Week2/list/hogwarts/hogwarts.py
@@ -0,0 +1,4 @@
+students = ["Herimione Granger", "Harry Potter", "Ron Weasley"]
+print(students[0])  # Output: Herimione Granger
+print(students[1])  # Output: Harry Potter
+print(students[2])  # Output: Ron Weasley
